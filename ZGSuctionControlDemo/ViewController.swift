17c0ea47bdfd6f0817636560147460eee5150882
developerZGJ <zhaoguojin@bogon>
2017-08-01 17:13:09 +0800

    Initial Commit

---
+++ ZGSuctionControlDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZGSuctionControlDemo
+//
+//  Created by 赵国进 on 2017/8/1.
+//  Copyright © 2017年 上海途宝网络科技. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
